b348f05b453740261abd7a8de04f9c5511a5bea9
wilvers <juan.ignacio.wilvers@gmail.com>
2023-06-08 21:12:44 -0300

    Server constructor

---
+++ models/server.js
@@ -0,0 +1,22 @@
+
+const express = require("express");
+const http = require('http');
+const  socketio  = require("socket.io");
+
+
+class Server {
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT
+
+        //http server
+        this.server = http.createServer(this.app)
+
+        //config sockets
+        this.io = socketio( this.server, {/* configuraciones */ })
+
+    }
+
+    
+}
