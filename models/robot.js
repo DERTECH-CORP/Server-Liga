ba9302c2caaba1a0d9062ea71308ce021c8fdb24
wilvers <juan.ignacio.wilvers@gmail.com>
2023-06-09 18:42:21 -0300

    captain and type add model robot

--- models/robot.js
+++ models/robot.js
@@ -2,32 +2,39 @@ const { Schema, model } = require("mongoose");
 
 
 const RobotSchema = Schema({
-    name:{
-        type : String,
-        required : true,
+    name: {
+        type: String,
+        required: true,
     },
-    de:{
-        type : Schema.Types.ObjectId,
-        ref : 'User',
-        required : true,
-        captain : String,
+    de: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
     },
-    description : {
-        type : String,
-        required : true,
+    description: {
+        type: String,
+        required: true,
     },
-    score:{
-        type : Number,
-        required : true,
+    captain: {
+        type: String,
+        required: true,
     },
-    verify:{
-        type : Boolean,
-        required : true
+    type: {
+        type: String,
+        required: true,
+    },
+    score: {
+        type: Number,
+        required: true,
+    },
+    verify: {
+        type: Boolean,
+        required: true,
     }
 })
 
-MessageSchema.method('toJSON', function(){
-    const { __v, ...object} = this.toObject();
+MessageSchema.method('toJSON', function () {
+    const { __v, ...object } = this.toObject();
     return object;
 })
 
